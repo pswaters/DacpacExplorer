3510bfe69aef712c7ff14389030d9b5668096333
Paul S Waters <pwaters@afs.net>
2019-05-09 14:59:45 -0400

    1. Added Stg schema, and Stg.TableOne to Sample Database Project.

---
+++ src/SampleDatabaseProject-01/dbo/Tables/TableOne.sql
@@ -0,0 +1,45 @@
+﻿CREATE TABLE [dbo].[TableOne]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [ColOne] NCHAR(10) NULL,
+    [ColTwo] NVARCHAR(50) NULL,
+    [ColThree] DATE NOT NULL DEFAULT GETUTCDATE()
+);
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Test DacPac Extended props',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TableOne',
+    @level2type = NULL,
+    @level2name = NULL
+GO
+EXEC sp_addextendedproperty @name = N'TableType',
+    @value = N'Reference',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TableOne',
+    @level2type = NULL,
+    @level2name = NULL
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Col One Desc',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TableOne',
+    @level2type = N'COLUMN',
+    @level2name = N'ColOne'
+GO 
+	EXEC sp_addextendedproperty @name = N'ScdType',
+    @value = N'HV',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TableOne',
+    @level2type = N'COLUMN',
+    @level2name = N'ColOne'
+Go
